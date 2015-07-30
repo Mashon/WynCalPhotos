a70e1fa15dbb7b16fb312807706e9dc93e2a5c7f
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-07-30 17:56:11 -0400

    home page added

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :events
   resources :pages
 
-  get '/', to: 'events#index'
+  root 'pages#home'
   get '/about', to: 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
