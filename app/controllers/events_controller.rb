cef11ff84b7a40b4630a105fc962fd85ec826fa0
Chris <chrisalan313@gmail.com>
2015-07-30 16:51:51 -0400

    add date input

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -79,6 +79,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :description)
+      params.require(:event).permit(:name, :description, :start_date)
     end
 end
