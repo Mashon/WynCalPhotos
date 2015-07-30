8669a2adc846224741bc63d432fda4c5b2b823e3
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-07-30 08:04:40 -0400

    comments scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730114547) do
+ActiveRecord::Schema.define(version: 20150730120311) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["event_id"], name: "index_comments_on_event_id"
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
