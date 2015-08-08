5e9e41946134cc60c10353d6b5bb8ab6f7ee8e7c
Chris <chrisalan313@gmail.com>
2015-08-08 16:11:50 -0400

    Add search method to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,11 @@ class Event < ActiveRecord::Base
   has_many :reviews
   validates :description, :name, presence: true
 
+  scope :search, ->(term) { where("name LIKE ? OR description LIKE ?", wrap(term), wrap(term) ) }
+
+  private
+
+  def self.wrap(term)
+    "%#{term}%"
+  end
 end
